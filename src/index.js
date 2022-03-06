690c91d176faeb0a72152723ed32341940ca4500
Olga Zhuk <z-o-v2005@yandex.ru>
2022-03-06 13:01:42 +0300

    task complete 60%

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let pairs = {
+    [')']:'(',
+    ['}']:'{',
+    ['|']:'|',
+    [']']:'[',
+  };
+
+  let array = [];
+ for (let i=0; i < str.length; i++) {
+   let sym = str[i];
+   bracketsConfig.forEach((v) => {
+    if (v[0] === sym) {
+      array.push(sym);
+        if (pairs[sym] === sym) {
+          array.pop();
+          }
+        }
+        else {
+          let top = array[array.length - 1];
+          if (array.length === 0){
+             return false;
+          }
+          else {
+            if (pairs[sym] === top){
+              array.pop()
+            }
+            else {
+              return false;
+            }
+           }
+          }
+         })
+        }
+          return array.length === 0;
+   }
+  
